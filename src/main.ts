e1dbfc8cea85e98bfd5c3bbd53629f6f51d113c2
Unknowncata <133138517+Unknowncata@users.noreply.github.com>
2025-01-27 12:25:59 -0600

    published!

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ import "./eventListener/eventListener";
 // import * as mc from "@minecraft/server";
 // import { DynamicProperty } from "./data/dynamicProperty";
 
-// DynamicProperty.fetchData();
-
 // mc.world.afterEvents.chatSend.subscribe(() => {
 //   DynamicProperty.resetDynamicData();
 //   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:dynamicData")}`);
-
-//   // mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:storedBlocks")}`);
-//   // mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:storedBlocksGameID")}`);
 // });
