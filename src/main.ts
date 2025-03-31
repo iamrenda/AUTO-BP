0dd89a1a11a641bd27b9334576c0a3f59408713a
Unknowncata <133138517+Unknowncata@users.noreply.github.com>
2025-03-31 09:18:53 +0900

    final

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import "./eventListener/eventListener";
 //////////////////////////////////////
 //////////////////////////////////////
 //////////////////////////////////////
-import * as mc from "@minecraft/server";
-import { giveItems } from "./utilities/utilities";
+// import * as mc from "@minecraft/server";
+// import { giveItems } from "./utilities/utilities";
 // import { DynamicProperty } from "./data/dynamicProperty";
 
-mc.world.afterEvents.chatSend.subscribe(() => {
-  //   DynamicProperty.resetDynamicData();
-  //   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:dynamicData")}`);
-  //   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:storedBlocks")}`);
-  //   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:gameData")}`);
-  giveItems("Bridger");
-});
+// mc.world.afterEvents.chatSend.subscribe(() => {
+//   DynamicProperty.resetDynamicData();
+//   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:dynamicData")}`);
+//   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:storedBlocks")}`);
+//   mc.world.sendMessage(`${mc.world.getDynamicProperty("auto:gameData")}`);
+//     giveItems("Bridger");
+// });
